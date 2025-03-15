5b68a755715a41ac57424af65e7a37d9fc5d4876
zaynab-20 <zatoloye@gmail.com>
2025-03-15 21:07:26 +0100

    commit

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,13 +5,13 @@ const { createProduct, getAllProducts, getOneProduct, updateProduct, deleteProdu
 const upload = require('../utils/multer')
 
 
-router.post('/createProduct', upload.single('image'), createProduct)
+router.post('/createProduct', upload.single('productImage'), createProduct)
 
 router.get('/getAllProducts',  getAllProducts)
 
 router.get('/getOneProduct/:id', getOneProduct)
 
-router.put('/updateProduct/:id',upload.single('image'), updateProduct)
+router.put('/updateProduct/:id',upload.single('productImage'), updateProduct)
 
-router.delete('/deleteProduct/:id',upload.single('image'), deleteProduct)
+router.delete('/deleteProduct/:id',upload.single('productImage'), deleteProduct)
 module.exports = router
--- utils/multer.js
+++ utils/multer.js
@@ -1,32 +1,30 @@
-
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
+    destination: (req, file, cb) => {
         cb(null, './uploads')
     },
-    filename: (res, file, cb) =>{
-        const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1E9)
-        const ext = file.mimetype.split('/')[1]
-        cb(null, file.fieldname + '-' + uniqueSuffix + "." + ext)
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
     }
-});
-const fileFilter = (req, file, cb) =>{
-    if (file.mimetype.startsWith('image/')) {
+})
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype.startsWith('image/')) {
         cb(null, true)
     }else {
-        cb(new Error('Invalid file type, image only '))
+        cb(new Error('invalid file type, please enter only image'))
     }
-};
+}
 
 const limits = {
-    limits: 1024 * 1024 * 10
-};
+    limits : 1024 * 1024 * 10
+}
 
 const upload = multer({
     storage,
     fileFilter,
     limits
-});
+})
+
 
 module.exports = upload
